829c8d9f2c420777272c495251544ad3726d7ff0
Jericho <mackulinm@gmail.com>
2023-04-15 17:53:16 +0200

    add basic character movement physics

--- Styles.unity/Assets/Styles.Game/scripts/CharacterPhysics2D.cs
+++ Styles.unity/Assets/Styles.Game/scripts/CharacterPhysics2D.cs
@@ -1,23 +1,54 @@
 ﻿using System;
+using Styles.Game.scripts.physics;
 using UnityEngine;
 
 namespace Styles.Game.scripts
 {
+
+[RequireComponent(typeof(BoxCollider2D))]
 public class CharacterPhysics2D : MonoBehaviour
 {
+    [SerializeField] private Vector2 _motor;
     [SerializeField] private float _baseMoveSpeed;
+    [SerializeField] private BoxCollider2D _collider;
+    [SerializeField] private LayerMask _collisionMask;
 
     private Transform _transform;
     private Vector2 _velocity;
+    private Vector2 _velocityDelta;
 
+    private BoxPhysics2D _boxPhysics;
+    
     private void Awake()
     {
         _transform = transform;
+        _boxPhysics = new BoxPhysics2D(_collider, 0.1f, _collisionMask);
     }
 
     private void Update()
     {
-        _transform.position += (Vector3)_velocity * Time.deltaTime;
+        _velocityDelta = _velocity * Time.deltaTime;
+        bool horizontalCollision = _boxPhysics.CheckHorizontalCollision(transform.position, _velocityDelta.x, out var horizontalHit);
+        bool verticalCollision = _boxPhysics.CheckVerticalCollision(transform.position, _velocityDelta.y, out var verticalHit);
+
+        if (horizontalCollision)
+        {
+            // snap to to hit
+            var horizontalSnap =  Mathf.Sign(_velocityDelta.x) * horizontalHit.distance;
+            Debug.Log($"hitDistance: {horizontalHit.distance} horizontalSnap: {horizontalSnap}");
+            _transform.position += Vector3.right * horizontalSnap;
+            _velocityDelta.x = 0;
+        }
+
+        if (verticalCollision)
+        {
+            var verticalSnap = Mathf.Sign(_velocityDelta.y) * verticalHit.distance;
+            Debug.Log($"hitDistance: {verticalHit.distance} verticalSnap: {verticalSnap}");
+            _transform.position += Vector3.up * verticalSnap;
+            _velocityDelta.y = 0;
+        }
+        
+        _transform.position += (Vector3)_velocityDelta;
     }
 
     public void Move(Vector2 direction)
---
+++ Styles.unity/Assets/Styles.Game/scripts/physics/BoxPhysics2D.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+
+namespace Styles.Game.scripts.physics
+{
+public class BoxPhysics2D
+{
+    private BoxCollider2D _collider;
+
+    private int _verticalRayCount;
+    private int _horizontalRayCount;
+
+    private float _maxRayGap;
+    private float _colliderHalfWidth;
+    private float _colliderHalfHeight;
+
+    private LayerMask _collisionMask;
+    private Vector2 _offset;
+    private Vector2 _depthSize;
+
+    public BoxPhysics2D(BoxCollider2D collider, float maxRayGap, LayerMask collisionMask, float depth = 0.00f, float shrinkSizeMag = 0.90f)
+    {
+        _collisionMask = collisionMask;
+        _collider = collider;
+        
+        var shrinkedSize = _collider.size * shrinkSizeMag;
+        _depthSize = Vector2.one * (1 - shrinkSizeMag) / 2;
+        
+        Debug.Log($"size: {_collider.size} shrinked: {shrinkedSize} depthSize: {_depthSize}");
+        
+        _verticalRayCount = (int) Mathf.Ceil(shrinkedSize.y / maxRayGap) + 1;
+        _horizontalRayCount = (int) Mathf.Ceil(shrinkedSize.x / maxRayGap) + 1;
+
+        _maxRayGap = maxRayGap;
+
+        _colliderHalfWidth = shrinkedSize.x / 2;
+        _colliderHalfHeight = shrinkedSize.y / 2;
+
+        _offset = _collider.offset;
+    }
+
+    public bool CheckHorizontalCollision(Vector2 position, float deltaX, out RaycastHit2D hit)
+    {
+        hit = new RaycastHit2D();
+        
+        if (deltaX == 0)
+            return false;
+
+        var dir = Mathf.Sign(deltaX);
+
+        for (int i = 0; i < _horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = position + Vector2.up * (_maxRayGap * i) + Vector2.right * (dir * _colliderHalfWidth)- Vector2.up * (_colliderHalfHeight) + _offset;
+            Vector2 rayDirection = Vector2.right * dir;
+            float rayLength = Math.Abs(deltaX) + _depthSize.x;
+
+            Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.green);
+
+            hit = Physics2D.Raycast(rayOrigin, rayDirection, rayLength, _collisionMask);
+            
+            if(hit)
+            {
+                Debug.Log($"rayLength: {rayLength} deltaX: {deltaX}");
+                hit.distance -= _depthSize.x;
+                Debug.DrawLine(rayOrigin, hit.point, Color.red);
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    public bool CheckVerticalCollision(Vector2 position, float deltaY, out RaycastHit2D hit)
+    {
+        hit = new RaycastHit2D();
+        
+        if (deltaY == 0)
+            return false;
+
+        var dir = Mathf.Sign(deltaY);
+
+        for (int i = 0; i < _verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = position + Vector2.right * (_maxRayGap * i) - Vector2.right * (_colliderHalfWidth) 
+                + Vector2.up * (dir * _colliderHalfHeight) + _offset;
+            Vector2 rayDirection = Vector2.up * dir;
+            float rayLength = Math.Abs(deltaY) + _depthSize.y;
+
+            Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.green);
+
+            hit = Physics2D.Raycast(rayOrigin, rayDirection, rayLength, _collisionMask);
+            
+            if(hit)
+            {
+                Debug.Log($"rayLength: {rayLength} deltaY: {deltaY}");
+                hit.distance -= _depthSize.y;
+                Debug.DrawLine(rayOrigin, hit.point, Color.red);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+}
